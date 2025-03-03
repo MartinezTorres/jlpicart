acf5be727a7596fbf40c6ae0eaa366e816a54065
JLPiCart <noreply@jlpicart>
2025-03-04 00:34:05 +0100

    lots of changes

--- sw/src/roms/roms.h
+++
@@ -1,2 +0,0 @@
-extern uint8_t __in_flash("games") spelunk_en_rom[];
-extern uint8_t __in_flash("games") pwnd_rom[];
--- sw/src/vga/vga.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-
-void vga320_init( std::function<const uint8_t *(uint16_t)> get_framebuffer_line = std::function<const uint8_t *(uint16_t)>() );
-
-//extern uint8_t framebuffer[240][320];
